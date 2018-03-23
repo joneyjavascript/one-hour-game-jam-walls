3ef31e53d910242e028c2e9d0af1631a7d478016
joneyjs <joney.jsantos@gmail.com>
2018-03-22 22:35:54 -0300

    first commit, menu, credits and game scene

---
+++ Assets/Scenes/Credits/Scripts/CreditsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour {
+
+    public void BackToMenu()
+    {
+        GotoScene("MenuScene");
+    }
+    
+    public void GotoScene(string nestSceneName) {
+        SceneManager.LoadScene(nestSceneName);
+    }
+
+}
---
+++ Assets/Scenes/Game/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    
+    private float _oldTimeScale;
+    private bool paused = false;
+
+    public void PauseGame()
+    {
+        if (paused)
+        {
+            throw new UnityException("Game is paused, not pause again.");
+        }
+
+        _oldTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+        paused = true;        
+    }
+
+    public void ResumeGame()
+    {
+        if (!paused)
+        {
+            throw new UnityException("Game is not paused, can not resume it.");
+        }
+
+        Time.timeScale = _oldTimeScale;
+        paused = false;
+       
+    }
+
+    public void BackToMenu()
+    {
+        GotoScene("MenuScene");
+    }
+    
+    public void GotoScene(string nestSceneName) {
+        SceneManager.LoadScene(nestSceneName);
+    }
+
+}
